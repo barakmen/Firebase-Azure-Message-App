224f12e1f79a8220c8ed0aa9b53b616c7620d411
Whats now <barakmen@outlook.com>
2018-01-04 15:31:36 +0200

    Connect to firebase

--- MessagesManager/MessagesManager/Class1.cs
+++ MessagesManager/MessagesManager/Class1.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Firebase.Database;
+using Firebase.Database.Query;
+using Newtonsoft.Json;
 
 namespace MessagesManager
 {
-    public class Class1
+
+
+    public class Program
     {
+        public async Task<Dinosaur> Run()
+        {
+
+
+            var firebase = new FirebaseClient("https://azure-sending-messages.firebaseio.com/");
+            var dinos = await firebase
+              .Child("dinosaurs")
+              .OnceAsync<Dinosaur>();
+
+            foreach (var dino in dinos)
+            {
+                Console.WriteLine($"{dino.Key} is {dino.Object.Height}m high.");
+            }
+
+            return null;
+        }
+    }
+    public class Dinosaur
+    {
+        [JsonProperty(PropertyName = "Height")]
+        public double Height { get; set; }
     }
 }
---
+++ MessagesManager/WebApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MessagesManager;
+using System;
+
+namespace WebApp
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run().Wait();
+            Console.ReadLine();
+        }
+    }
+}
